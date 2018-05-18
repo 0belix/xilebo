a71e5df0016eebb4141feb9f29131ee40600d28c
Pierre Johansson <obelix.epost@gmail.com>
2018-05-18 15:45:44 +0200

    saving...

--- public/js/protocol.js
+++ public/js/protocol.js
@@ -8,18 +8,77 @@ $(document).ready(() => {
   $('.containerFlex').on('click', (e) => { calculate_bonus_driver('_home') })
   $('.containerFlex').on('click', (e) => { calculate_bonus_driver('_away') })
   $('.containerFlex').on('click', (e) => { hider(e) })
+  $('.containerFlex').on('click', (e) => { lane_changer(e) })
   // $('.containerFlex').on('click', calc)
 })
 
+function lane_changer(e) {
+  let one = document.querySelectorAll('.lane_1')
+  let two = document.querySelectorAll('.lane_2')
+  let three = document.querySelectorAll('.lane_3')
+  let four = document.querySelectorAll('.lane_4')
+  one.forEach((e) => {
+    e.classList.remove('lane_1')
+    e.classList.add('lane_2')
+  })
+  two.forEach((e) => {
+    e.classList.remove('lane_2')
+    e.classList.add('lane_1')
+  })
+  three.forEach((e) => {
+    e.classList.remove('lane_3')
+    e.classList.add('lane_4')
+  })
+  four.forEach((e) => {
+    e.classList.remove('lane_4')
+    e.classList.add('lane_3')
+  })
+}
+
 function hider(e) {
-  if (e.target.id === 'h16') {
-    e.target.classList.toggle('hide')
-    for (let i = 1; i <= 8; i++) {
-      document.querySelector('#d' + i + 'h16_home').classList.toggle('hide')
+  let theID = e.target.id
+  let theRE = /(^h([1-9]|1[0-5])_home$|^h([1-9]|1[0-5])_away$)/
+  if (theID === 'h16_home' || theID === 'h16_away') {
+    for (let x = 0; x < 2; x++) {
+      let where = x === 0 ? '_home' : '_away'
+      document.querySelector('#h16' + where).classList.toggle('hide')
+      for (let i = 1; i <= 8; i++) {
+        document.querySelector('#d' + i + 'h16' + where).classList.toggle('hide')
+      }
+      document.querySelector('#h16_sum' + where).classList.toggle('hide')
+      document.querySelector('#h16_tot' + where).classList.toggle('hide')
     }
-    document.querySelector('#h16_sum_home').classList.toggle('hide')
-    document.querySelector('#h16_tot_home').classList.toggle('hide')
-    document.querySelector('#h16_blank_home').classList.toggle('hide')
+    document.querySelector('#h16_time').classList.toggle('hide')
+  } else {
+    if (theID.match(theRE)) {
+      for (let x = 0; x < 2; x++) {
+        let where = x === 0 ? '_home' : '_away'
+        for (let j = 1; j <= 15; j++) {
+          if (theID === 'h' + j + '_home' || theID === 'h' + j + '_away') { continue }
+          document.querySelector('#h' + j + '' + where).classList.toggle('hide')
+          for (let i = 1; i <= 8; i++) {
+            document.querySelector('#d' + i + 'h' + j + '' + where).classList.toggle('hide')
+            if (j === 1) {
+              document.querySelector('#d' + i + '_sum' + where).classList.toggle('hide')
+              document.querySelector('#d' + i + '_bonus' + where).classList.toggle('hide')
+              document.querySelector('#d' + i + '_heats' + where).classList.toggle('hide')
+            }
+          }
+        document.querySelector('#h' + j + '_sum' + where).classList.toggle('hide')
+          document.querySelector('#h' + j + '_tot' + where).classList.toggle('hide')
+          if (x === 1) { document.querySelector('#h' + j + '_time').classList.toggle('hide') }
+        }
+        document.querySelector('#sum' + where).classList.toggle('hide')
+        document.querySelector('#bonus' + where).classList.toggle('hide')
+        document.querySelector('#heats' + where).classList.toggle('hide')
+        document.querySelector('#sum_sum' + where).classList.toggle('hide')
+        document.querySelector('#sum_bonus' + where).classList.toggle('hide')
+        document.querySelector('#sum_heats' + where).classList.toggle('hide')
+        document.querySelector('#tot_sum' + where).classList.toggle('hide')
+        document.querySelector('#tot_bonus' + where).classList.toggle('hide')
+        document.querySelector('#tot_heats' + where).classList.toggle('hide')
+      }
+    }
   }
 }
 
